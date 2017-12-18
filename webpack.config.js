f5b7af4f1ac06eac0ccb3ea44f3b40bc56a43e22
vidocco <jack.mk.thibaut@gmail.com>
2017-12-18 18:03:14 +0100

    chore: code styling

--- webpack.config.js
+++ webpack.config.js
@@ -6,17 +6,20 @@ const config = {
   entry: './path/to/my/entry/file.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'my-first-webpack.bundle.js'
+    filename: 'my-first-webpack.bundle.js',
   },
   module: {
     rules: [
-      { test: /\.txt$/, use: 'raw-loader' }
-    ]
+      {
+        test: /\.txt$/,
+        use: 'raw-loader',
+      },
+    ],
   },
   plugins: [
     new webpack.optimize.UglifyJsPlugin(),
-    new HtmlWebpackPlugin({template: './src/index.html'})
-  ]
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+  ],
 };
 
 module.exports = config;
