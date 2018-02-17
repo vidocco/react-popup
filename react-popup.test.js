1fda9638633a728565f25bba6010fa519c4bb25d
vidocco <jack.mk.thibaut@gmail.com>
2018-02-17 16:35:13 +0100

    refactor: improved test functionality

--- react-popup.test.js
+++ react-popup.test.js
@@ -12,17 +12,19 @@ describe('React Popup', () => {
   it('should appear in DOM as <input type="button"/>', () => {
     const component = shallow(<ReactPopup/>)
     expect(component).toMatchSnapshot();
+    expect(component.find('.popup-display').exists()).toBe(false);
+    expect(component.find('input').exists()).toBe(true);
   });
 
   it('should display when button is clicked and hide when overlay is clicked', () => {
     const component = shallow(<ReactPopup/>);
-    expect(component.find('.popup-display')).toHaveLength(0);
+    expect(component.find('.popup-display').exists()).toBe(false);
     expect(component).toMatchSnapshot();
     component.find('input').simulate('click');
-    expect(component.find('.popup-display')).toHaveLength(1);
+    expect(component.find('.popup-display').exists()).toBe(true);
     expect(component).toMatchSnapshot();
     component.find('.popup-overlay').simulate('click');
-    expect(component.find('.popup-display')).toHaveLength(0);
+    expect(component.find('.popup-display').exists()).toBe(false);
     expect(component).toMatchSnapshot();
   });
 });
