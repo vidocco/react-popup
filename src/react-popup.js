42cff30d2ab5e60b8c5d0c1c657220991a745154
vidocco <jack.mk.thibaut@gmail.com>
2018-02-22 00:14:47 +0100

    refactor: reduced cognitive complexity and lines of code

--- src/react-popup.js
+++ src/react-popup.js
@@ -53,25 +53,58 @@ export default class ReactPopUp extends React.Component {
     });
   }
 
-  render() {
-    /**
-     * Mapping the passed styles (if they have been defined in their corresponding
-     * props) and/or setting the styles to their default values (if they don't).
-     */
+  /**
+   * Conditionally displays the popup if the state is set to popped.
+   * @param { object } styles an object with the styles for overlay and popup.
+   * @returns { node | bool } popup JSX or false.
+   */
+  renderPopUp = (styles) => {
+    return this.state.popped && (
+      <div className="popup-display">
+        <div
+          style={styles.overlay}
+          onClick={this.pop}
+          className="popup-overlay"
+        >
+        </div>
+        <div className="popup" style={styles.popup}>
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+
+  /**
+   * checks for the existance of an object, if it does exist and has a value it
+   * returns said value; else, it returns a default value.
+   * @param { object } obj style object to check.
+   * @param { string } prop property of said object to check.
+   * @param { string } def default value of said property (in case it's null).
+   * @returns { string } value for said prop or default value.
+   */
+  renderStyles = (obj, prop, def) => {
+    return (obj && obj[prop]) || def
+  }
+
+  /**
+   * Mapping the passed styles (if they have been defined in their corresponding
+   * props) and/or setting the styles to their default values (if they don't).
+   */
+  render () {
     const styles = {
       overlay: {
-        width: `${ (this.props.overlay && this.props.overlay.width) || '100%'}`,
-        height: `${ (this.props.overlay && this.props.overlay.height) || '100%' }`,
-        backgroundColor: `${ (this.props.overlay && this.props.overlay.backgroundColor) || 'rgba(0,0,0,0.4)' }`,
+        width: `${ this.renderStyles(this.props.overlay, 'width', '100%') }`,
+        height: `${ this.renderStyles(this.props.overlay, 'height', '100%') }`,
+        backgroundColor: `${ this.renderStyles(this.props.overlay, 'backgroundColor', 'rgba(0,0,0,0.4)') }`,
         position: 'absolute',
         zIndex: 1,
         ...this.props.overlay,
       },
       popup: {
-        width: `${ (this.props.popup && this.props.popup.width) || '450px'}`,
-        height: `${ (this.props.popup && this.props.popup.height) || '650px' }`,
-        backgroundColor: `${ (this.props.popup && this.props.popup.backgroundColor) || 'white' }`,
-        boxShadow: `${ (this.props.popup && this.props.popup.boxShadow) || '0 0 10px 0 rgba(0, 0, 0, 0.5)' }`,
+        width: `${ this.renderStyles(this.props.popup, 'width', '450px')}`,
+        height: `${ this.renderStyles(this.props.popup, 'height', '650px') }`,
+        backgroundColor: `${ this.renderStyles(this.props.popup, 'backgroundColor', 'white') }`,
+        boxShadow: `${ this.renderStyles(this.props.popup, 'boxShadow', '0 0 10px 0 rgba(0, 0, 0, 0.5)') }`,
         position: 'absolute',
         zIndex: 2,
         ...this.props.popup,
@@ -82,22 +115,7 @@ export default class ReactPopUp extends React.Component {
     };
     return (
       <div className="react-popup" style={{ zIndex: -1 }}>
-        {this.state.popped
-          ? (
-            <div className="popup-display">
-              <div
-                style={styles.overlay}
-                onClick={this.pop}
-                className="popup-overlay"
-              >
-              </div>
-              <div className="popup" style={styles.popup}>
-                {this.props.children}
-              </div>
-            </div>
-          ) : (
-            null
-          )}
+        {this.renderPopUp(styles)}
         <input style={{zIndex: -1, ...styles.button}} type="button" onClick={this.pop} value={this.props.buttonText || 'show'} />
       </div>
     );
